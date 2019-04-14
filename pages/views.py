95a08d1b230768ec361fa1b482e2a7103aa95b86
Nadia89M <nadia.mohamed89@gmail.com>
2019-04-14 17:37:21 +0100

    Update send email setup

--- pages/views.py
+++ pages/views.py
@@ -25,8 +25,8 @@ def contact(request):
             send_mail(
               'Contact Inquiry',
               'There has been an inquiry. Sign into the admin panel for more info.',
-              'nadia.mohamed89@gmail.com',
-              ['nadia.mohamed89@gmail.com'],
+              'valentino.grancagnolo@gmail.com',
+              ['valentino.grancagnolo@gmail.com'],
             )
             return thanks(request)
     return render(request, 'pages/contact.html', {'form': form})
